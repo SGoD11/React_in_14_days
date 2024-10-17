c90df4d2538ce421f13332c0772d29faf9eda270
subhajit dhar <subhajit13dhar@gmail.com>
2024-10-17 13:46:20 +0530

    added some changes css

--- 04-state-management/src/components/CounterApp.jsx
+++ 04-state-management/src/components/CounterApp.jsx
@@ -18,9 +18,9 @@ export default function CounterApp() {
                 <h1 className="text-center">Counter App</h1>
                 <p className="fs-1"> {counter}</p>
                 <div className="buttons d-flex gap-2 justify-content-center">
-                    <button className="btn btn-success" onClick={increment}> <i className="bi bi-plus-circle-fill"></i> </button>
-                    <button className="btn btn-success" onClick={reset}><i className="bi bi-arrow-clockwise"></i></button>
                     <button className="btn btn-success" onClick={decrement}><i className="bi bi-dash-circle-fill"></i></button>
+                    <button className="btn btn-success" onClick={reset}><i className="bi bi-arrow-clockwise"></i></button>
+                    <button className="btn btn-success" onClick={increment}> <i className="bi bi-plus-circle-fill"></i> </button>
                 </div>
             </div>
         </div>
